1cf687946e131deddb320a86f2f978fbe4299c12
Aaron Arismendi <arismendiaaron@gmail.com>
2021-09-03 01:08:48 -0400

    Finalizado

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-function generator(){
-
-}
-let pronoun = ["I", "You", "He", "She", "It", "We"];
-let adj = ["Angry", "Asleep", "Awake", "Bright", "Delicate", "Dirty"];
-let noun = ["Doctor", "Man", "Woman", "Child", "Dog", "Kid"];
-
-var name = pronoun[Math.floor(Math.random() * firstPart.length)] + " " + adj[Math.floor(Math.random() * firstPart.length)] + " " + noun[Math.floor(Math.random() * secondPart.length)];
-
-alert(name);
-
-if (document.getElementById("name")) {
+function generator() {
+   
+    let pronoun = ["I", "You", "He", "She", "It", "We"];
+    let adj = ["Angry", "Asleep", "Awake", "Bright", "Delicate", "Dirty"];
+    let noun = ["Doctor", "Man", "Woman", "Child", "Dog", "Kid"];
+    let tld = [".com", ".es", ".ve", ".org", ".net"]
+    
+    var name = pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * noun.length)] + tld[Math.floor(Math.random() * tld.length)];
+    
+    if (document.getElementById("name")) {
     document.getElementById("placeholder").removeChild(document.getElementById("name"));
     }
-
-var element = document.createElement("div");
-element.setAttribute("id", "name");
-element.appendChild(document.createTextNode(name));
-document.getElementById("placeholder").appendChild(element);
+   
+    var element = document.createElement("div");
+    element.setAttribute("id", "name");
+    element.appendChild(document.createTextNode(name));
+    document.getElementById("placeholder").appendChild(element);
+    }
